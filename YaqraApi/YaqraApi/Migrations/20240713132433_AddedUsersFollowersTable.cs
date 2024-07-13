e6a746efe4f0a6eae0cb32f12753ae63efa638f1
OmarNaru <nadaelomr391@gmail.com>
2024-07-13 16:34:12 +0300

    implemented following users feature

---
+++ YaqraApi/YaqraApi/Migrations/20240713132433_AddedUsersFollowersTable.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YaqraApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUsersFollowersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UsersFollowers",
+                columns: table => new
+                {
+                    FollowerId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FollowedId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersFollowers", x => new { x.FollowerId, x.FollowedId });
+                    table.ForeignKey(
+                        name: "FK_UsersFollowers_AspNetUsers_FollowedId",
+                        column: x => x.FollowedId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_UsersFollowers_AspNetUsers_FollowerId",
+                        column: x => x.FollowerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersFollowers_FollowedId",
+                table: "UsersFollowers",
+                column: "FollowedId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersFollowers_FollowerId",
+                table: "UsersFollowers",
+                column: "FollowerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsersFollowers");
+        }
+    }
+}
